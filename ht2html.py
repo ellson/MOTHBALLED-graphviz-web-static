3eca520ba36f25894e76a4401d42c0f11ad188a4
ellson <devnull@localhost>
2004-09-13 17:01:34 +0000

    new Download pages

--- ht2html.py
+++ ht2html.py
@@ -37,10 +37,14 @@ fout.write('''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
 for page in pageset:
   menuitem = page.split('.')[0]
-  if basename == menuitem:
-    fout.write('\t<tr><td bgcolor="#c0ffc0">' + menuitem + '</td></tr>\n')
+  if len(menuitem.split('_')) > 1:
+     menuname = '&nbsp;&nbsp;' + menuitem.split('_')[1]
   else:
-    fout.write('\t<tr><td><a href="' + menuitem + '.html">' + menuitem + '</a></td></tr>\n')
+     menuname = menuitem
+  if basename == menuitem:
+    fout.write('\t<tr><td bgcolor="#c0ffc0">' + menuname + '</td></tr>\n')
+  else:
+    fout.write('\t<tr><td><a href="' + menuitem + '.html">' + menuname + '</a></td></tr>\n')
 
 fout.write('''\t</table> 
 </td><td valign="top">
