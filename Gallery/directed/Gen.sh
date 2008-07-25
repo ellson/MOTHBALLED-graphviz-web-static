149d20e3733681f3a7166970942520f5af161dcf
ellson <devnull@localhost>
2008-07-25 16:46:09 +0000

    rename .dot -> .gv

--- Gallery/directed/Gen.sh
+++ Gallery/directed/Gen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-for i in *.dot.txt
+for i in *.gv.txt
 do
 	echo $i
-	j=${i%.dot.txt}
+	j=${i%.gv.txt}
 	# can't use neato -s -n2 because it does strange things to 
-	# the records in datastruct.dot
+	# the records in datastruct.gv
 	dot -Tpng -Gsize="7,7" $i -o $j.png
 	convert -resize 160x160 $j.png $j.small.png
 	# dot -Tpng -Gsize="1.75,1.75" $i -o $j.small.png
